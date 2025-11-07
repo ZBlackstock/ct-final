40053a1471d1cc90892d539a354beae1248fc8cd
ZBlackstock <40592313@live.napier.ac.uk>
2025-11-07 12:45:51 +0000

    Reformatted Isenbart BT

--- Assets/Rotate.cs
+++ Assets/Rotate.cs
@@ -6,10 +6,30 @@ public class Rotate : MonoBehaviour
 {
     [SerializeField] private float speed = 0;
     private Vector3 rotation;
+    private float speedUpRate;
+    [SerializeField] private float maxSpeed;
+    private bool speedUp;
+
     void Update()
     {
-        rotation = new Vector3(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, 
+        rotation = new Vector3(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y,
             transform.rotation.eulerAngles.z + speed * Time.deltaTime);
         transform.rotation = Quaternion.Euler(rotation);
+
+        if (speedUp && Mathf.Abs(speed) < maxSpeed)
+        {
+            speed += speedUpRate;
+        }
+        else
+        {
+            speedUp = false;
+        }
+    }
+
+    public void SpeedUp(float rate)
+    {
+        speedUpRate = rate;
+        speedUp = true;
+        Debug.Log("WORK CUNT");
     }
 }
---
+++ Assets/Scripts/BT_Tasks/BT_SpeedUpClock.cs
@@ -0,0 +1,19 @@
+using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Enemy AI - calls SpeedUp() in small clock hand Rotate
+public class BT_SpeedUpClock : EnemyAction
+{
+    public float speedRate;
+    public Rotate clockRotate;
+
+    public override void OnStart()
+    {
+        Debug.Log("what the fuck");
+        clockRotate.SpeedUp(speedRate);
+    }
+}
